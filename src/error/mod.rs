ee2bd902f811deb367020bd55c3283245c0c5fff
BlockListed <44610569+BlockListed@users.noreply.github.com>
2023-02-04 19:59:52 +0100

    implement thiserror for better errors

--- src/error/mod.rs
+++ src/error/mod.rs
@@ -1,10 +1,15 @@
 pub type HexaFreezeResult<T> = Result<T, HexaFreezeError>;
 
-#[derive(Debug, PartialEq, Eq)]
+#[derive(Debug, PartialEq, Eq, thiserror::Error)]
 pub enum HexaFreezeError {
+    #[error("Epoch is too far in the future!")]
     EpochInTheFuture,
+    #[error("Epoch is too far in the past!")]
     EpochTooFarInThePast,
+    #[error("The node_id is too large / greater than 4095!")]
     NodeIdTooLarge,
+    #[error("Your clock jumped backwards in time!")]
     ClockWentBackInTime,
+    #[error("You've generated more than 9,223,372,036,854,775,807 ids!")]
     Surpassed64BitLimit,
 }
